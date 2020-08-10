cd4d59d331ca14eedd847c93ff0f253a42a0aa3e
amman <aman.kapoor@1mg.com>
2020-08-11 02:16:33 +0530

    Updates

---
+++ precache-manifest.1fa74ea044ed9a78bf68ce208d8abb58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a07b4b71273dd5616b83460b39c73dc",
+    "url": "/whiteBoard/index.html"
+  },
+  {
+    "revision": "9e0baa24d8f48a786e2b",
+    "url": "/whiteBoard/static/css/main.c70b2a86.chunk.css"
+  },
+  {
+    "revision": "b3b70c32f53b7f640be5",
+    "url": "/whiteBoard/static/js/2.57a7043f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whiteBoard/static/js/2.57a7043f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0baa24d8f48a786e2b",
+    "url": "/whiteBoard/static/js/main.28dbb61e.chunk.js"
+  },
+  {
+    "revision": "c903b9e7d98776998282",
+    "url": "/whiteBoard/static/js/runtime-main.1d68bb44.js"
+  }
+]);
